03541baa882439d207730410dc081bf207bfe6b3
Fortunes <fortunes@Jordans-MacBook-Pro-3.local>
2018-09-23 16:45:11 -0600

    Connected to Firebase

--- src/store/sagas/burgerBuilder.js
+++ src/store/sagas/burgerBuilder.js
@@ -6,7 +6,7 @@ import * as actions from "../actions";
 export function* initIngredientsSaga(action) {
   try {
     const response = yield axios.get(
-      "https://react-my-burger.firebaseio.com/ingredients.json"
+      "https://react-my-burger-a45c6.firebaseio.com/ingredients.json"
     );
     yield put(actions.setIngredients(response.data));
   } catch (error) {
