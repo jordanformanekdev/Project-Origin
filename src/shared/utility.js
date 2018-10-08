0bf91f307bbc699faab594066f9c13d63cd80627
Fortunes <fortunes@Jordans-MacBook-Pro-3.local>
2018-10-08 11:20:09 -0600

    SignUpPage #comment Added Password Confirmation to SignUpPage

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -5,33 +5,44 @@ export const updateObject = (oldObject, updatedProperties) => {
     };
 };
 
-export const checkValidity = ( value, rules ) => {
+export const checkValidity = ( value, state, currentControl ) => {
     let isValid = true;
-    if ( !rules ) {
+        
+    if ( !state.controls[currentControl].validation ) {
         return true;
     }
 
-    if ( rules.required ) {
+    if ( state.controls[currentControl].validation.required ) {
         isValid = value.trim() !== '' && isValid;
     }
 
-    if ( rules.minLength ) {
-        isValid = value.length >= rules.minLength && isValid
+    if ( state.controls[currentControl].validation.minLength ) {
+        isValid = value.length >= state.controls[currentControl].validation.minLength && isValid
     }
 
-    if ( rules.maxLength ) {
-        isValid = value.length <= rules.maxLength && isValid
+    if ( state.controls[currentControl].validation.maxLength ) {
+        isValid = value.length <= state.controls[currentControl].validation.maxLength && isValid
     }
 
-    if ( rules.isEmail ) {
+    if ( state.controls[currentControl].validation.isEmail ) {
         const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
         isValid = pattern.test( value ) && isValid
     }
 
-    if ( rules.isNumeric ) {
+    if ( state.controls[currentControl].validation.isNumeric ) {
         const pattern = /^\d+$/;
         isValid = pattern.test( value ) && isValid
     }
 
     return isValid;
-}
+};
+
+export const confirmPasswords = ( password, passwordConfirm ) => {
+
+    if ( password === passwordConfirm ) {
+      return true;
+    } else {
+      return false;
+    }
+
+};
